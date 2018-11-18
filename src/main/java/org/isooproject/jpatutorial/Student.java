eff049f7ab9a3f1937670a9ef4a07387342906c6
leesoo <isoo7510@gmail.com>
2018-11-18 14:24:24 +0900

    initial commit & db connect success

---
+++ src/main/java/org/isooproject/jpatutorial/Student.java
@@ -0,0 +1,18 @@
+package org.isooproject.jpatutorial;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String passportNumber;
+
+}
