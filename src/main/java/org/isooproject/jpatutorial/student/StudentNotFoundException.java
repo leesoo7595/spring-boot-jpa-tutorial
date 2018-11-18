97527848b8d43a7d4cd676e3413cf6b1b5fcacf1
leesoo <isoo7510@gmail.com>
2018-11-18 14:52:19 +0900

    custom exception & find each student

---
+++ src/main/java/org/isooproject/jpatutorial/student/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+package org.isooproject.jpatutorial.student;
+
+class StudentNotFoundException extends Exception {
+
+    StudentNotFoundException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/org/isooproject/jpatutorial/student/StudentResource.java
+++ src/main/java/org/isooproject/jpatutorial/student/StudentResource.java
@@ -2,9 +2,11 @@ package org.isooproject.jpatutorial.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentResource {
@@ -14,6 +16,17 @@ public class StudentResource {
 
     @GetMapping("/students")
     public List<Student> retrieveAllStudents() {
+
         return studentRepository.findAll();
+    }
+
+    @GetMapping("/students/{id}")
+    public Student retreiveStudent(@PathVariable long id) throws StudentNotFoundException {
+
+        Optional<Student> student = studentRepository.findById(id);
+
+        if (!student.isPresent())
+            throw new StudentNotFoundException("There is no students");
+        return student.get();
     }
 }
