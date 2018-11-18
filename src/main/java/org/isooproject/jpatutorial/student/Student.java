3e10462885daa6fd970707dc3d51e3cde38230ee
leesoo <isoo7510@gmail.com>
2018-11-18 14:35:34 +0900

    add getter & setter

--- src/main/java/org/isooproject/jpatutorial/student/Student.java
+++ src/main/java/org/isooproject/jpatutorial/student/Student.java
@@ -15,4 +15,28 @@ public class Student {
 
     private String passportNumber;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
 }
